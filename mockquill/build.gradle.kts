811e1af9f6e44d17b5f47cadb56f25865f7f0541
melarozz <melarozz18@gmail.com>
2025-03-03 21:20:50 +0700

    fix: gitignore didn't work lol

--- mockquill/build.gradle.kts
+++ mockquill/build.gradle.kts
@@ -10,14 +10,11 @@ repositories {
 }
 
 dependencies {
-    // ByteBuddy
     implementation("net.bytebuddy:byte-buddy:1.14.7")
     implementation("net.bytebuddy:byte-buddy-agent:1.14.7")
 
-    // JUnit 4 support (needed for Assert and @Test)
     testImplementation("junit:junit:4.13.2")
 
-    // Other dependencies
     implementation("org.openjdk.jol:jol-core:0.17")
     implementation("org.jfree:jfreechart:1.5.3")
     implementation("org.jfree:jcommon:1.0.24")
