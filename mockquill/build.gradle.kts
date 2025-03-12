5c54812e732971c804f0f52f050a2e5e32ae6e96
melarozz <melarozz18@gmail.com>
2025-03-12 11:36:54 +0700

    fix: not used libraries

--- mockquill/build.gradle.kts
+++ mockquill/build.gradle.kts
@@ -14,10 +14,6 @@ dependencies {
     implementation("net.bytebuddy:byte-buddy-agent:1.14.7")
 
     testImplementation("junit:junit:4.13.2")
-
-    implementation("org.openjdk.jol:jol-core:0.17")
-    implementation("org.jfree:jfreechart:1.5.3")
-    implementation("org.jfree:jcommon:1.0.24")
 }
 
 tasks.test {
